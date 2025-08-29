a0d3f5a5730cd234693e379e0f53576f7d47ef25
islem gouicem <gouicemislam0@gmail.com>
2025-08-29 20:26:04 +0100

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
-    base: mode === "production" ? "/snt_website/" : "/",
+    base: "/",
     plugins: [tailwindcss(), react()],
     optimizeDeps: {
       include: ["lucide-react"], // make sure icons are optimized properly
